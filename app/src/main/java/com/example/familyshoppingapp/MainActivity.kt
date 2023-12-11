c43c5b8c76219726c937fa84c924c6aa8862141b
Linus Ilbratt <linusilbratt@gmail.com>
2023-12-11 13:19:35 +0100

    connecting the adapter and activity together

--- app/src/main/java/com/example/familyshoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/familyshoppingapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.familyshoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ProductAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val productList = mutableListOf<Product>()
+
+        adapter = ProductAdapter(productList) { position ->
+            adapter.removeItem(position)
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/example/familyshoppingapp/ProductAdapter.kt
+++ app/src/main/java/com/example/familyshoppingapp/ProductAdapter.kt
@@ -7,11 +7,13 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+class ProductAdapter(private val productList: MutableList<Product>, private val onDeleteClicked: (Int) -> Unit) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textViewProductName: TextView = view.findViewById(R.id.productNameTextView)
         val buttonAddToCart: Button = view.findViewById(R.id.buttonAddToCart)
+        val buttonDelete: Button = view.findViewById(R.id.buttonDelete)
+        val buttonEdit: Button = view.findViewById(R.id.buttonEdit)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
@@ -23,7 +25,32 @@ class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adap
         val currentItem = productList[position]
         holder.textViewProductName.text = currentItem.name
 
+        // Change alpha value if a product is added to the cart
+        if (currentItem.isAdded) {
+            holder.itemView.alpha = 0.5f
+        } else {
+            holder.itemView.alpha = 1.0f
+        }
+
+        holder.buttonAddToCart.setOnClickListener {
+            currentItem.isAdded = !currentItem.isAdded
+            notifyItemChanged(position)
+        }
+
+        holder.buttonDelete.setOnClickListener {
+            onDeleteClicked(position)
+        }
+
+        holder.buttonEdit.setOnClickListener {
+            //onEditClicked(currentItem)
+        }
+
     }
 
     override fun getItemCount() = productList.size
+
+    fun removeItem(position: Int) {
+        productList.removeAt(position)
+        notifyItemRemoved(position)
+    }
 }
