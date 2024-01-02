b20cdbeaf546e481f40270948591450e8ba09f39
Linus Ilbratt <linusilbratt@gmail.com>
2024-01-02 09:44:43 +0100

    new data class for the hidden gem places

---
+++ app/src/main/java/com/example/familyshoppingapp/HiddenGem.kt
@@ -0,0 +1,10 @@
+package com.example.familyshoppingapp
+
+data class HiddenGem(
+    val name: String,
+    val description: String,
+    val latitude: Double,
+    val longitude: Double,
+    val imageUrl: String?,
+    val tag: String
+)
