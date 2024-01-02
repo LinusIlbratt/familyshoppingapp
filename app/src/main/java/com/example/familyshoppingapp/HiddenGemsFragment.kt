d784f0af4bcfb223157608a399b2e0db013340f9
Linus Ilbratt <linusilbratt@gmail.com>
2024-01-02 10:12:08 +0100

    Created adapter, fragment and layout for each hidden gem, the layout contains only a text view for the titel for testing purposes

--- app/src/main/java/com/example/familyshoppingapp/HiddenGemsFragment.kt
+++ app/src/main/java/com/example/familyshoppingapp/HiddenGemsFragment.kt
@@ -5,21 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HiddenGemsFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var hiddenGemsAdapter: HiddenGemsAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hidden_gems, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_hidden_gems, container, false)
 
+        recyclerView = view.findViewById(R.id.recyclerViewGemList) // Referera till RecyclerView i din layout
+
+        // Här kan du hämta din data för HiddenGem-objekten
+        val hiddenGemsList = listOf<HiddenGem>() // Ersätt med din data
+
+        hiddenGemsAdapter = HiddenGemsAdapter(hiddenGemsList)
+        recyclerView.adapter = hiddenGemsAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
+    }
 }
