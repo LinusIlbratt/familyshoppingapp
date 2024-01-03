6d33cb6bcf7ef7aca22214869ec0400e23687353
Linus Ilbratt <linusilbratt@gmail.com>
2024-01-03 06:53:07 +0100

    started to set up the fragment and created a layout

--- app/src/main/java/com/example/familyshoppingapp/HiddenGemDetailFragment.kt
+++ app/src/main/java/com/example/familyshoppingapp/HiddenGemDetailFragment.kt
@@ -1,4 +1,35 @@
 package com.example.familyshoppingapp
 
-class HiddenGemDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HiddenGemDetailFragment : Fragment() {
+    private lateinit var hiddenGem: HiddenGem
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            hiddenGem = it.getParcelable(ARG_HIDDEN_GEM) ?: throw IllegalArgumentException("Hidden Gem is required")
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate din layout och visa information om hiddenGem här
+    }
+
+    companion object {
+        private const val ARG_HIDDEN_GEM = "hidden_gem"
+
+        fun newInstance(hiddenGem: HiddenGem): HiddenGemDetailFragment {
+            val args = Bundle().apply {
+                putParcelable(ARG_HIDDEN_GEM, hiddenGem)
+            }
+            return HiddenGemDetailFragment().apply {
+                arguments = args
+            }
+        }
+    }
 }
