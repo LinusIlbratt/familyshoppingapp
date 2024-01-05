36d7bae3988077b2f5ce53f426680885ed7f3658
Linus Ilbratt <linusilbratt@gmail.com>
2024-01-05 14:17:32 +0100

    removed navController and went with my old method instead, created a new adapter for the new search fragment and connected those, implemented a SearchView in the layout and created a basic layout for the recycleview

--- app/src/main/java/com/example/familyshoppingapp/SearchHiddenGemsFragment.kt
+++ app/src/main/java/com/example/familyshoppingapp/SearchHiddenGemsFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchHiddenGemsFragment : Fragment() {
 
-    // Initialize variables here
+    // Andra variabler och funktioner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +23,32 @@ class SearchHiddenGemsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val searchView = view.findViewById<SearchView>(R.id.search_view)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView_search_results)
 
-        // Initialize views here
+        val adapter = SearchHiddenGemsAdapter(emptyList())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    // Här kan du anropa din sökfunktion och uppdatera adaptern
+                    //adapter.updateData(sökresultaten)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
     }
 
-    // Add more functions
+
+    private fun searchHiddenGems(query: String) {
+        // Hämta data från Firestore baserat på query
+        // Exempel: Firestore-db.collection("hidden_gems").whereEqualTo("tag", query).get()...
+    }
 }
+
