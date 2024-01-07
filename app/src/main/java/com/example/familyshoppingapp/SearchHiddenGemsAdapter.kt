fdae6fc405075eec4aac4d72705404ac160dee85
Linus Ilbratt <linusilbratt@gmail.com>
2024-01-07 21:00:31 +0100

    can now search and open a hidden gem that is shared public and fixed a bug where you had to press onBack twice to get back to the fragment, appeared I had a double onItemClicked in my adapter

--- app/src/main/java/com/example/familyshoppingapp/SearchHiddenGemsAdapter.kt
+++ app/src/main/java/com/example/familyshoppingapp/SearchHiddenGemsAdapter.kt
@@ -1,29 +1,37 @@
 package com.example.familyshoppingapp
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SearchHiddenGemsAdapter(private var hiddenGems: List<HiddenGem>) :
+
+class SearchHiddenGemsAdapter(private var hiddenGems: List<HiddenGem>, private val onItemClicked: (HiddenGem) -> Unit) :
     RecyclerView.Adapter<SearchHiddenGemsAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, private val onItemClicked: (HiddenGem) -> Unit) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.text_view_search_result)
-        // Fler views kan läggas till här
+
+        fun bind(hiddenGem: HiddenGem) {
+            textView.text = hiddenGem.name
+
+            itemView.setOnClickListener {
+                Log.d("!!!", "Klickade på namn: ${hiddenGem.name}")
+                onItemClicked(hiddenGem)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.search_hidden_gem_items, parent, false)
-        return ViewHolder(view)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_hidden_gem_items, parent, false)
+        return ViewHolder(view, onItemClicked)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val hiddenGem = hiddenGems[position]
-        holder.textView.text = hiddenGem.name
-        // Sätt andra värden på views här
+        holder.bind(hiddenGem)
     }
 
     override fun getItemCount() = hiddenGems.size
